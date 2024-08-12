a4d9b956f643f3abf53740302f0c6522d7a0ce78
cupomdavezoficial <gabriiellfr@hotmail.com>
2024-08-12 15:49:26 -0300

    Logger Brazilian timezone

--- utils/logger.util.js
+++ utils/logger.util.js
@@ -1,13 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+const { format } = require('date-fns-tz');
 
+// Path to the log file
 const logFile = path.join(__dirname, '../logs', 'app.log');
 
+// São Paulo time zone
+const TIMEZONE = 'America/Sao_Paulo';
+
+// Logger function
 const logger = (message) => {
-    const timestamp = new Date().toISOString();
-    fs.appendFile(logFile, `${timestamp} - ${message}\n`, (err) => {
-        if (err) console.error('Error writing to log file', err);
-    });
+    try {
+        // Get the current time in São Paulo time zone
+        const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss', {
+            timeZone: TIMEZONE,
+        });
+        // Write log message to file
+        fs.appendFile(logFile, `${timestamp} - ${message}\n`, (err) => {
+            if (err) console.error('Error writing to log file', err);
+        });
+    } catch (err) {
+        console.error('Error in logger function:', err);
+    }
 };
 
 module.exports = logger;
